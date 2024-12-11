27de33bdcbcd836e8d0b038fd6e20c8d1d8cc0a7
Tam Thong <120663808+tamthong1115@users.noreply.github.com>
2024-12-11 07:56:58 +0700

    handle correctly loading when fetch roles

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAppContext } from "../context/AppContext";
+import LoadingComponent from "./Loading/Loading.tsx";
 
 interface ProtectedRouteProps {
     requiredRoles?: string[];
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ requiredRoles = [] }) => {
-    const { isLoggedIn, roles } = useAppContext();
+    const { isLoggedIn, roles, roleLoading, loading } = useAppContext();
 
+    if (roleLoading || loading) {
+        return <LoadingComponent isLoading={true} />;
+    }
 
     const hasRequiredRoles = requiredRoles.every((role) => roles.includes(role));
+
+    console.log(`isLoggedIn: ${isLoggedIn}, hasRequiredRoles: ${hasRequiredRoles}`);
 
     return isLoggedIn && hasRequiredRoles ? <Outlet /> : <Navigate to="/" />;
 };
